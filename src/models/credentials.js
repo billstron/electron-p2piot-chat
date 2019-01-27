b18fd1f7e843e0c4b90caae51f18690e663c7b0f
Bill Burke <billstron@gmail.com>
2019-01-27 10:40:42 -0500

    storing creds in memory

--- src/models/credentials.js
+++ src/models/credentials.js
@@ -5,26 +5,41 @@ const storage = persistent();
 let credentials = null;
 
 function Factory() {
+
+  const store = {
+    uid: storage.get('uid') || '',
+    certificate: storage.get('certificate') || '',
+    publicKey: null,
+  };
+
+  function setPublicKey() {
+    if (store.certificate.length > 0) {
+      const key = new RSA(store.certificate);
+      store.publicKey = key.exportKey('public');
+    } else {
+      store.publicKey = '';
+    }
+  }
+  setPublicKey();
+
   const self = {
     get uid() {
-      return storage.get('uid') || null;
+      return store.uid;
     },
     set uid(uid) {
       storage.set('uid', uid);
+      store.uid = uid;
     },
     get certificate() {
-      return storage.get('certificate') || null;
+      return store.certificate;
     },
     set certificate(cert) {
       storage.set('certificate', cert);
+      store.certificate = cert;
+      setPublicKey();
     },
     get publicKey() {
-      const cert = storage.get('certificate');
-      if (cert === null || cert.length === 0) {
-        return null;
-      }
-      const key = new RSA(cert);
-      return key.exportKey('public');
+      return store.publicKey;
     },
     set publicKey(key) {
       throw new Error('not allowed');
