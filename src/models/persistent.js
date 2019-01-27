28dac2e6cd0f6e377e895971c6832538aadf8e1d
Bill Burke <billstron@gmail.com>
2019-01-27 08:33:19 -0500

    added persistent to profile

---
+++ src/models/persistent.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+const electron = window.require('electron');
+
+export default function(opts = {}, defaults = {}) {
+  const { fname = 'config.json' } = opts;
+  const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+  const filePath = path.join(userDataPath, fname);
+  let data = defaults;
+  try {
+    data = JSON.parse(fs.readFileSync(filePath));
+  } catch(error) {
+    // console.error(error); // eslint-disable-line no-console
+    data = defaults;
+  }
+
+  const self = {
+    get(key) {
+      return data[key];
+    },
+
+    set(key, value) {
+      data[key] = value;
+      fs.writeFileSync(filePath, JSON.stringify(data));
+    },
+  };
+
+  return self;
+}
---
+++ src/views/Profile.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="profile">
+    <h1>My Profile</h1>
+    <input type="text" v-model="uid"/>
+    <button v-on:click="setUid">Set UID</button>
+    <textarea class="certificate" v-model="certificate" />
+    <button v-on:click="setCertificate">Set Cert</button>
+  </div>
+</template>
+
+<style lang="scss">
+div.profile {
+  input[type="text"] {
+    width: 100%;
+  }
+  button {
+    float: left;
+  }
+  textarea {
+    width: 100%;
+    height: 5rem;
+  }
+}
+
+</style>
+
+<script>
+import persistent from '../models/persistent';
+const model = persistent();
+
+export default {
+  name: 'profile',
+  data: () => ({
+    uid: '',
+    publicKey: '',
+    certificate: '',
+  }),
+  methods: {
+    setUid() {
+      model.set('uid', this.uid);
+    },
+    setPrivateKey() {
+      this.data.publicKey = 'butts';
+      model.set('certificate', this.certificate);
+    }
+  },
+  created() {
+    this.uid = model.get('uid') || '';
+    this.certificate = model.get('certificate') || '';
+    this.publicKey = '';
+  },
+};
+</script>
